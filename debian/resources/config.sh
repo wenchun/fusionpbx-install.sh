1ec2ce493cc8e8da48cf53e18f8cc8d629dcc23a
wenchun <wenchun@users.noreply.github.com>
2019-06-24 12:02:36 +0800

    Update config.sh

--- debian/resources/config.sh
+++ debian/resources/config.sh
@@ -1,8 +1,8 @@
 
 # FusionPBX Settings
-domain_name=192.168.1.163          # hostname, ip_address or a custom value
+domain_name=ip_address          # hostname, ip_address or a custom value
 system_username=admin           # default username admin
-system_password=admin@1234          # random or a custom value
+system_password=random          # random or a custom value
 system_branch=master            # master, stable
 
 # FreeSWITCH Settings
@@ -16,7 +16,7 @@ switch_tls=true                 # true or false
 database_password=random        # random or a custom value (safe characters A-Z, a-z, 0-9)
 database_repo=official          # PostgreSQL official, system, 2ndquadrant
 database_version=latest         # requires repo official
-database_host=pgdb         # hostname or IP address
+database_host=127.0.0.1         # hostname or IP address
 database_port=5432              # port number
 database_backup=false           # true or false
 
