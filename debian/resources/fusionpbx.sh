de8683ff8b779f0630c261c2eb710a461aba70f6
wenchun <wenchun@users.noreply.github.com>
2019-06-24 15:53:43 +0800

    Update local config for fusionpbx.sh

--- debian/resources/fusionpbx.sh
+++ debian/resources/fusionpbx.sh
@@ -19,8 +19,8 @@ if [ .$system_branch = .'master' ]; then
 	verbose "Using master"
 	branch=""
 else
-	system_major=$(git ls-remote --heads https://github.com/fusionpbx/fusionpbx.git | cut -d/ -f 3 | grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f1)
-	system_minor=$(git ls-remote --tags https://github.com/fusionpbx/fusionpbx.git $system_major.* | cut -d/ -f3 |  grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f2)
+	system_major=$(git ls-remote --heads https://github.com/wenchun/fusionpbx.git | cut -d/ -f 3 | grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f1)
+	system_minor=$(git ls-remote --tags https://github.com/wenchun/fusionpbx.git $system_major.* | cut -d/ -f3 |  grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f2)
 	system_version=$system_major.$system_minor
 	verbose "Using version $system_version"
 	branch="-b $system_version"
@@ -31,6 +31,6 @@ mkdir -p /var/cache/fusionpbx
 chown -R www-data:www-data /var/cache/fusionpbx
 
 #get the source code
-git clone $branch https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
+git clone $branch https://github.com/wenchun/fusionpbx.git /var/www/fusionpbx
 chown -R www-data:www-data /var/www/fusionpbx
 chmod -R 755 /var/www/fusionpbx/secure
